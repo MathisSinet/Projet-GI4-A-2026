e38b21b802a5a749f4fe76e785970e0ef2072d20
cytech <cytech@student-laptop>
2026-05-18 19:43:34 +0200

    Début de l'architecture grâce au diagramme de classe fait.

---
+++ src/flowly/viewcontroller/InspectorController.java
@@ -0,0 +1,18 @@
+package flowly.viewcontroller;
+
+import javafx.scene.control.TextArea;
+import flowly.geometry.VoronoiCell;
+import flowly.geometry.DelaunayTriangle;
+
+public class InspectorController {
+    private TextArea statsTextArea;
+
+    public InspectorController() {
+    }
+
+    public void displayCellStats(VoronoiCell cell) {
+    }
+
+    public void displayTriangleStats(DelaunayTriangle triangle) {
+    }
+}
