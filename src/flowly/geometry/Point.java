025642a3f47fcf9bc325179f25bfad2488994333
nabilathr <tahrinabil@cy-tech.fr>
2026-05-18 20:51:25 +0200

    Début de l'architecture grâce au diagramme de classe fait.

---
+++ src/flowly/geometry/Point.java
@@ -0,0 +1,36 @@
+package flowly.geometry;
+
+public class Point {
+    protected double x;
+    protected double y;
+    
+    
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+    
+    public void length(Point p) {
+    	
+    }
+    
+    public void distance(Point p) {
+    	
+    }
+}
